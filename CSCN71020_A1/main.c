4908e49c41de11f503370e03087627016947619d
Yinkaalams <adeyinkaalamutu0@gmail.com>
2023-02-08 01:26:23 -0500

    Fixed bug in the code

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -16,7 +16,7 @@ void main() {
 
 	printf("Enter operation number: ");
 	scanf_s("%1o", &inputNum);
-	printf("Welcome");
+	printf("Welcome\n");
 
 	switch (inputNum)
 	{
@@ -44,13 +44,15 @@ void printOptions() {
 }
 
 void add() {
-	double num1, num2, result;
+	double num1, num2,num3, result;
 	printf("Enter the first value:");
 	scanf_s("%lf", &num1);
 	printf("Enter the second value:");
 	scanf_s("%lf", &num2);
+	printf("Enter the third value:");
+	scanf_s("%lf", &num3);
 	result = num1 + num2;
-	printf("%lf + %lf = %lf\n", num1, num2, result);
+	printf("%lf + %lf = %lf\n", num1, num2, num3, result);
 }
 void subtract() {
 	double num1, num2, result;
